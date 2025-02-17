9f708fd8a17a889d562a15295561ff183c3ab334
Je2001-om <gehadomer6031@gmail.com>
2025-02-17 02:16:52 +0200

    edit

--- main.js
+++ main.js
@@ -1,17 +1,54 @@
 
+const signupForm = document.getElementById("regform");
+const loginForm = document.getElementById("logform");
+const formTitle = document.getElementById("form-title");
+const toggleToLogin = document.getElementById("login");
+const toggleToSignup = document.getElementById("signup");
+
+const signupBtn = document.getElementById("signup1");
+const loginBtn = document.getElementById("login1");
 
 
-const name = document.getElementById("name");
-const email = document.getElementById("email");
-const paasword = document.getElementById("paasword");
-const form = document.getElementById("form");
-const name_error = document.getElementById("name_error");
+function toggleForms() {
+    signupForm.classList.toggle("hidden");
+    loginForm.classList.toggle("hidden");
+
+    formTitle.innerText = signupForm.classList.contains("hidden") ? "Log In" : "Sign Up";
+}
+
+toggleToSignup.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+});
+
+toggleToLogin.addEventListener("click", (e) => {
+    e.preventDefault();
+    toggleForms();
+});
 
 
 
-form.addEventListener('submit', (e) => { 
 
-    if (name.value() === '' || name.value == null) { 
-        e.preventDefault(); 
+signupBtn.addEventListener("click", () => {
+    let username = document.getElementById("signup-username").value;
+    let password = document.getElementById("signup-password").value;
+
+    if (username && password) {
+        alert("Account created successfully!");
+        toggleForms(); 
+    } else {
+        alert("Please enter a username and password.");
+    }
+});
+
+
+loginBtn.addEventListener("click", () => {
+    let username = document.getElementById("login-email").value;
+    let password = document.getElementById("login-password").value;
+
+    if (username && password) {
+        alert("Login successful!");
+    } else {
+        alert("Invalid email or password.");
     }
 });
